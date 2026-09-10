0ffceaabf8f11580aeaa9e95832ec4b3b37afcdf
mmm-source-create <mmurielcagigal@gmail.com>
2026-09-10 04:24:08 +0200

    Add files via upload

--- src/content/territorios/index.js
+++ src/content/territorios/index.js
@@ -8,24 +8,4 @@ import r4 from "./europa-oriental.js";
 import r5 from "./islas-norte.js";
 export const HISTORIA_TERRITORIOS = Object.freeze({...r0,...r1,...r2,...r3,...r4,...r5,...fronteras,...ampliacion});
 
-export const FUENTES_TERRITORIOS = {
-  "Armenia cilicia": [{titulo:"Monarquía cilicia: cronología y bibliografía",url:"https://en.wikipedia.org/wiki/List_of_monarchs_of_the_Armenian_Kingdom_of_Cilicia"}],
-  Chipre: [{titulo:"Cambridge: A History of Cyprus — Pedro I",url:"https://www.cambridge.org/core/books/abs/history-of-cyprus/peter-i-135969/433D1C9C6EFE77BAB71F18F7E850A3D5"}],
-  Kildare: [{titulo:"Dictionary of Irish Biography: Gerald FitzGerald",url:"https://www.dib.ie/index.php/biography/fitzgerald-gerald-gearoid-mor-a3148"}],
-  Ormond: [{titulo:"Dictionary of Irish Biography: James Butler",url:"https://www.dib.ie/biography/butler-james-a1259"}],
-  Lucan: [{titulo:"Dictionary of Irish Biography: Patrick Sarsfield",url:"https://www.dib.ie/index.php/biography/sarsfield-patrick-a7924"}],
-  "Condado de Clancare": [{titulo:"Dictionary of Irish Biography: Donal MacCarthy Mór",url:"https://www.dib.ie/index.php/biography/maccarthy-mor-donal-a5138"}],
-  "Vizcondado de Mayo": [{titulo:"Dictionary of Irish Biography: Grace O’Malley",url:"https://www.dib.ie/biography/omalley-grainne-grace-granuaile-a6886"}],
-  Georgia: [{titulo: "Encyclopaedia Iranica: relaciones entre Georgia e Irán", url: "https://www.iranicaonline.org/articles/georgia-ii-history/"}],
-  "Kartli-Kajetia": [{titulo: "Encyclopaedia Iranica: Heraclio II", url: "https://www.iranicaonline.org/articles/erekle-ii/"}],
-  Kartli: [{titulo: "Encyclopaedia Iranica: Kartli", url: "https://www.iranicaonline.org/articles/kartli/"}],
-  Irlanda: [{titulo: "Dictionary of Irish Biography: Ruaidrí Ua Conchobair", url: "https://www.dib.ie/biography/ua-conchobair-ruaidri-a8725"}, {titulo: "Dictionary of Irish Biography: Hugh O’Neill", url: "https://www.dib.ie/index.php/biography/oneill-hugh-a6962"}],
-  Leinster: [{titulo: "Dictionary of Irish Biography: Diarmait Mac Murchada", url: "https://www.dib.ie/biography/mac-murchada-diarmait-macmurrough-dermot-a5075"}],
-  Trinacria: [{titulo: "Treccani: Federico de Aragón, rey de Sicilia", url: "https://www.treccani.it/enciclopedia/federico-iii-d-aragona-re-di-sicilia_(Dizionario-Biografico)/"}],
-  Transilvania: [{titulo: "Leibniz-Institut: Gábor Bethlen", url: "https://www.biolex.ios-regensburg.de/BioLexViewview.php?start=186"}],
-  Castilla: [{titulo:"Ministerio de Cultura: Corona de Castilla",url:"https://tesauros.cultura.gob.es/tesauros/contextosculturales/1172659.html"}],
-  León: [{titulo:"Ministerio de Cultura: Corona de Castilla",url:"https://tesauros.cultura.gob.es/tesauros/contextosculturales/1172659.html"}],
-  "Corona de Castilla": [{titulo:"Ministerio de Cultura: Corona de Castilla",url:"https://tesauros.cultura.gob.es/tesauros/contextosculturales/1172659.html"}],
-  Carintia: [{titulo:"Treccani: Enrico, conte del Tirolo e duca di Carinzia",url:"https://www.treccani.it/enciclopedia/enrico-conte-del-tirolo-e-duca-di-carinzia_(Enciclopedia-Italiana)/"}],
-  Sajonia: [{titulo:"British Museum: Maurice, Elector of Saxony",url:"https://www.britishmuseum.org/collection/term/BIOG152590"}],
-};
+export { FUENTES_TERRITORIOS } from "../sources.js";
