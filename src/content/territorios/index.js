fb024c9793c0fe1a3867688082b53f0c84e350e2
mmm-source-create <mmurielcagigal@gmail.com>
2026-09-12 18:53:28 +0200

    Add files via upload

--- src/content/territorios/index.js
+++ src/content/territorios/index.js
@@ -1,3 +1,4 @@
+import paisesBajos from "./paises-bajos-v211.js";
 import ampliacion from "./fronteras-v28.js";
 import casas from "./casas-v210.js";
 import fronteras from "./fronteras-v272.js";
@@ -7,6 +8,6 @@ import r2 from "./imperio.js";
 import r3 from "./italia.js";
 import r4 from "./europa-oriental.js";
 import r5 from "./islas-norte.js";
-export const HISTORIA_TERRITORIOS = Object.freeze({...r0,...r1,...r2,...r3,...r4,...r5,...fronteras,...ampliacion,...casas});
+export const HISTORIA_TERRITORIOS = Object.freeze({...r0,...r1,...r2,...r3,...r4,...r5,...fronteras,...ampliacion,...casas,...paisesBajos});
 
 export { FUENTES_TERRITORIOS } from "../sources.js";
